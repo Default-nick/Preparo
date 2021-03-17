f25b09607dc03ca144aa5c1b5a6281b8371fef6a
Default-nick <67932981+Default-nick@users.noreply.github.com>
2021-03-16 22:02:08 -0300

    adicionado menu para dispositivos mobile, ajustes de tamanho para responsividade

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from './components/navbar/header.jsx';
 import ReactDOM from 'react-dom';
 import Profile from './pages/profile/profile'
+import NavBarMobile from './components/navbarMobile/navBarMobile.jsx';
 
 ReactDOM.render (
     <React.StrictMode>
         <Header/>
         <Profile/>
+        <NavBarMobile/>
     </React.StrictMode>,
     document.getElementById('root')
 );
