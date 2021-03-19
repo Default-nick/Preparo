23b39476fcc6a4b9844e5b86943b8370f9a7bef9
Default-nick <67932981+Default-nick@users.noreply.github.com>
2021-03-18 22:53:01 -0300

    Adição de validação em diversos campos das páginas de Localização e Dados Básicos

--- src/pages/basicData/basicData-template.jsx
+++ src/pages/basicData/basicData-template.jsx
@@ -1,87 +1,77 @@
-import './basicData.css'
-import React from 'react'
+import './basicData.css';
+import React, {useState} from 'react';
+import {useForm} from 'react-hook-form';
 
-export default class BasicData extends React.Component {
-    constructor(props){
-        super(props)
-        this.state = {value: ''};
-        this.digitarPesquisa = this.digitarPesquisa.bind(this)
-        this.validar = this.validar.bind(this)
-    }
-// Validação para remover #
-    digitarPesquisa(event){
-        var auxiliar = event.target.value.replace(/[#]/g,"");
-        this.setState({
-            value: auxiliar
-        })
-    }
-// Validação para quantidade de caracteres
-    validar(event){
-        event.preventDefault();
-        if(this.state.value.length < 3){
-            alert ('Não há caracteres o suficiente para resultar numa busca.');
-        }
-        if(this.state.value.length > 140){
-            alert ('Número máximo de caracteres atingido.');
+export default function BasicData(){
+    const {register, handleSubmit, errors} = useForm();
+    const onSubmit = data => console.log(data);
+    const [value, setValue] = useState('');
+    function phoneValidate(phone){
+        setValue(phone);
+        if(value.length >= 10){
+            setValue(phone.replace(/^(\d\d)(\d{5})(\d{4}).*/,"($1) $2-$3"));
         }
     }
-    render () {
-        return(
-            <form className="editProfile">
-                <div className="cardProfileSection">
-                    <div className="firstSection">
-                        <span id="titleEditProfile">Foto do Perfil</span>
-                        <span id="textEditProfile">Adicione uma foto em seu perfil</span>
-                    </div>
-                    <div className="secondSection">
-                        <svg id="profilePic" stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 496 512" size="100" height="100" width="100" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm0 96c48.6 0 88 39.4 88 88s-39.4 88-88 88-88-39.4-88-88 39.4-88 88-88zm0 344c-58.7 0-111.3-26.6-146.5-68.2 18.8-35.4 55.6-59.8 98.5-59.8 2.4 0 4.8.4 7.1 1.1 13 4.2 26.6 6.9 40.9 6.9 14.3 0 28-2.7 40.9-6.9 2.3-.7 4.7-1.1 7.1-1.1 42.9 0 79.7 24.4 98.5 59.8C359.3 421.4 306.7 448 248 448z">
-                            </path>
-                        </svg>
-                        <button id="upPhoto" className="blueButton">Carregar foto</button>
-                    </div>
+    return(
+        <form onSubmit={handleSubmit(onSubmit)} className="editProfile">
+            <div className="cardProfileSection">
+                <div className="firstSection">
+                    <span id="titleEditProfile">Foto do Perfil</span>
+                    <span id="textEditProfile">Adicione uma foto em seu perfil</span>
                 </div>
-                <div className="cardProfileSection">
-                    <div className="firstSection">
-                        <span id="titleEditProfile">Email</span>
-                        <span id="textEditProfile">Você pode alterar seu email</span>
-                    </div>
-                    <form className="secondSection" onSubmit={this.validar}>
-                        <span id="textEditProfile">Email <span className="redEmphasis">*</span></span>
-                        <input className="inputProfile" placeholder="email@exemplo.com.br" required></input>
-                        <button id="upEmail" type="submit" value={this.state.value} onChange={this.digitarPesquisa} className="blueButton">Atualizar email</button>
-                    </form>
+                <div className="secondSection">
+                    <svg id="profilePic" stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 496 512" size="100" height="100" width="100" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm0 96c48.6 0 88 39.4 88 88s-39.4 88-88 88-88-39.4-88-88 39.4-88 88-88zm0 344c-58.7 0-111.3-26.6-146.5-68.2 18.8-35.4 55.6-59.8 98.5-59.8 2.4 0 4.8.4 7.1 1.1 13 4.2 26.6 6.9 40.9 6.9 14.3 0 28-2.7 40.9-6.9 2.3-.7 4.7-1.1 7.1-1.1 42.9 0 79.7 24.4 98.5 59.8C359.3 421.4 306.7 448 248 448z">
+                        </path>
+                    </svg>
+                    <button id="upPhoto" className="blueButton">Carregar foto</button>
                 </div>
-                <div className="cardProfileSection">
-                    <div className="firstSection">
-                        <span id="titleEditProfile">Instruções</span>
-                        <span id="textEditProfile">Preencha seus dados de perfil. Sempre mantenha seu telefone atualizado.</span>
-                    </div>
-                    <div className="secondSection">
-                        <span id="textEditProfile">Nome <span className="redEmphasis">*</span></span>
-                        <input className="inputProfile" placeholder="Digite seu nome" required></input>
-                        <span id="textEditProfile">Sobrenome <span className="redEmphasis">*</span></span>
-                        <input className="inputProfile" placeholder="Digite seu sobrenome" required></input>
-                        <span id="textEditProfile">Telefone <span className="redEmphasis">*</span></span>
-                        <input className="inputProfile" type="number" placeholder="(00) 00000-0000" required></input>
-                    </div>
+            </div>
+            <div className="cardProfileSection">
+                <div className="firstSection">
+                    <span id="titleEditProfile">Email</span>
+                    <span id="textEditProfile">Você pode alterar seu email</span>
                 </div>
-                <div className="cardProfileSection">
-                    <div className="firstSection">
-                        <span id="titleEditProfile">Links Pessoais</span>
-                        <span id="textEditProfile">Compartilhe seu perfil de outras plataformas aqui.</span>
-                    </div>
-                    <div className="secondSection">
-                        <span id="textEditProfile">Github <span className="redEmphasis">*</span></span>
-                        <input className="inputProfile" placeholder="http://github.com/abcdefghi" required></input>
-                        <span id="textEditProfile">Behance <span className="redEmphasis">*</span></span>
-                        <input className="inputProfile" placeholder="http://www.behance.net/abcdefghi" required></input>
-                        <span id="textEditProfile">Linkedin <span className="redEmphasis">*</span></span>
-                        <input className="inputProfile" placeholder="http://www.linkedin.com/in/abcdefghi" required></input>
-                    </div>
+                <div className="secondSection">
+                    <span id="textEditProfile">Email <span className="redEmphasis">*</span></span>
+                    <input className="inputProfile" name="emailInput" ref={register({ required: true })} placeholder="email@exemplo.com.br"></input>
+                    {errors.emailInput && <span className="redEmphasis">Você precisa inserir um email</span>}
+                    <button id="upEmail" type="submit" className="blueButton">Atualizar email</button>
                 </div>
-                <button id="submitFormButton">Enviar</button>
-            </form>
-        );
-    }
+            </div>
+            <div className="cardProfileSection">
+                <div className="firstSection">
+                    <span id="titleEditProfile">Instruções</span>
+                    <span id="textEditProfile">Preencha seus dados de perfil. Sempre mantenha seu telefone atualizado.</span>
+                </div>
+                <div className="secondSection">
+                    <span id="textEditProfile">Nome <span className="redEmphasis">*</span></span>
+                    <input className="inputProfile" type="text" name="nameInput" ref={register({ required: true })} placeholder="Digite seu nome"></input>
+                    {errors.nameInput && <span className="redEmphasis">Você precisa inserir pelo menos um nome</span>}
+                    <span id="textEditProfile">Sobrenome <span className="redEmphasis">*</span></span>
+                    <input className="inputProfile" type="text" name="surnameInput" ref={register({ required: true })} placeholder="Digite seu sobrenome"></input>
+                    {errors.surnameInput && <span className="redEmphasis">Você precisa inserir pelo menos um sobrenome</span>}
+                    <span id="textEditProfile">Telefone <span className="redEmphasis">*</span></span>
+                    <input className="inputProfile" maxLength="12" value={value} onChange={e => phoneValidate(e.target.value)} name="phoneNumber" ref={register({ required: true })} placeholder="(00) 00000-0000"></input>
+                    {errors.phoneNumber && <span className="redEmphasis">Você precisa inserir um número de telefone com ddd
+</span>}
+                </div>
+            </div>
+            <div className="cardProfileSection">
+                <div className="firstSection">
+                    <span id="titleEditProfile">Links Pessoais</span>
+                    <span id="textEditProfile">Compartilhe seu perfil de outras plataformas aqui.</span>
+                </div>
+                <div className="secondSection">
+                    <span id="textEditProfile">Github</span>
+                    <input className="inputProfile" placeholder="http://github.com/abcdefghi"></input>
+                    <span id="textEditProfile">Behance</span>
+                    <input className="inputProfile" placeholder="http://www.behance.net/abcdefghi"></input>
+                    <span id="textEditProfile">Linkedin</span>
+                    <input className="inputProfile" placeholder="http://www.linkedin.com/in/abcdefghi"></input>
+                </div>
+            </div>
+            <input type="submit" id="submitFormButton" label="Atualizar"/>
+        </form>
+    );
 }
--- src/pages/local/localization-template.jsx
+++ src/pages/local/localization-template.jsx
@@ -1,64 +1,79 @@
-import React from 'react'
+import React, {useState} from 'react';
+import {useForm} from 'react-hook-form';
 
-export default class Localization extends React.Component {
-    render () {
-        return(
-            <form className="editProfile">
-                <span id="profileTitle">Localização</span>
-                <div className="cardProfileSection">
-                    <div className="firstSection">
-                        <span id="titleEditProfile">Instruções</span>
-                        <span id="textEditProfile">Preencha seus dados de Localização. Sempre os mantenha atualizado.</span>
-                    </div>
-                    <div className="secondSection">
-                    <span id="textEditProfile">CEP <span className="redEmphasis">*</span></span>
-                        <input className="inputProfile" type="number" placeholder="12345-678" required></input>
-                        <span id="textEditProfile">Cidade <span className="redEmphasis">*</span></span>
-                        <input className="inputProfile" placeholder="Insira o nome de sua cidade atual" required></input>
-                        <span id="textEditProfile">Estado <span className="redEmphasis">*</span></span>
-                        <input type="text" className="inputProfile" list="stateSelect" placeholder="Selecione o Estado"/>
-                        <datalist id="stateSelect" placeholder="Selecione o Estado" required>
-                            <option>Selecione o Estado</option>
-                            <option>AC</option>
-                            <option>AL</option>
-                            <option>AM</option>
-                            <option>AP</option>
-                            <option>BA</option>
-                            <option>CE</option>
-                            <option>DF</option>
-                            <option>ES</option>
-                            <option>GO</option>
-                            <option>MA</option>
-                            <option>MG</option>
-                            <option>MS</option>
-                            <option>MT</option>
-                            <option>PA</option>
-                            <option>PB</option>
-                            <option>PE</option>
-                            <option>PI</option>
-                            <option>PR</option>
-                            <option>RJ</option>
-                            <option>RN</option>
-                            <option>RO</option>
-                            <option>RR</option>
-                            <option>RS</option>
-                            <option>SC</option>
-                            <option>SE</option>
-                            <option>SP</option>
-                            <option>TO</option>
-                        </datalist>
-                        <span id="textEditProfile">Bairro <span className="redEmphasis">*</span></span>
-                        <input className="inputProfile" placeholder="Insira o nome de seu bairro" required></input>
-                        <span id="textEditProfile">Endereço <span className="redEmphasis">*</span></span>
-                        <input className="inputProfile" placeholder="Insira seu endereço" required></input>
-                        <span id="textEditProfile" >Número <span className="redEmphasis">*</span></span>
-                        <input className="inputProfile" type="number" placeholder="Insira o número de sua residência" required></input>
-                        <span id="textEditProfile">Complemento <span className="redEmphasis">*</span></span>
-                        <input className="inputProfile" placeholder="Insira um complemento se achar necessário" required></input>
-                    </div>
-                </div>
-                <button id="submitFormButton">Enviar</button>
-            </form>
-        );
+export default function Localization(){
+    const {register, handleSubmit, errors} = useForm();
+    const onSubmit = data => console.log(data);
+    const [value, setValue] = useState('');
+    function cepValidate(cep){
+        setValue(cep);
+        if(value.length === 8){
+            setValue(cep.replace(/^(\d{5})(\d{3}).*/,"$1-$2"));
+        }
     }
+    return(
+        <form onSubmit={handleSubmit(onSubmit)} className="editProfile">
+            <span id="profileTitle">Localização</span>
+            <div className="cardProfileSection">
+                <div className="firstSection">
+                    <span id="titleEditProfile">Instruções</span>
+                    <span id="textEditProfile">Preencha seus dados de Localização. Sempre os mantenha atualizado.</span>
+                </div>
+                <div className="secondSection">
+                <span id="textEditProfile">CEP <span className="redEmphasis">*</span></span>
+                    <input className="inputProfile" name="cepInput" ref={register({ required: true })} type="text" maxLength="9" value={value} onChange={e => cepValidate(e.target.value)} placeholder="12345-678"></input>
+                    {errors.cepInput && <span className="redEmphasis">Você precisa inserir um CEP válido</span>}
+                    <span id="textEditProfile">Cidade <span className="redEmphasis">*</span></span>
+                    <input className="inputProfile" name="cityInput" ref={register({ required: true })} placeholder="Insira o nome de sua cidade atual"></input>
+                    {errors.cityInput && <span className="redEmphasis">Você precisa inserir o nome de sua cidade</span>}
+                    <span id="textEditProfile">Estado <span className="redEmphasis">*</span></span>
+                    <input type="text" className="inputProfile" name="stateInput" ref={register({ required: true })} list="stateSelect" placeholder="Selecione o Estado"/>
+                    {errors.stateInput && <span className="redEmphasis">Você precisa inserir o nome de seu estado</span>}
+                    <datalist id="stateSelect" width="1500px" placeholder="Selecione o Estado">
+                        <option>Selecione o Estado</option>
+                        <option>AC</option>
+                        <option>AL</option>
+                        <option>AM</option>
+                        <option>AP</option>
+                        <option>BA</option>
+                        <option>CE</option>
+                        <option>DF</option>
+                        <option>ES</option>
+                        <option>GO</option>
+                        <option>MA</option>
+                        <option>MG</option>
+                        <option>MS</option>
+                        <option>MT</option>
+                        <option>PA</option>
+                        <option>PB</option>
+                        <option>PE</option>
+                        <option>PI</option>
+                        <option>PR</option>
+                        <option>RJ</option>
+                        <option>RN</option>
+                        <option>RO</option>
+                        <option>RR</option>
+                        <option>RS</option>
+                        <option>SC</option>
+                        <option>SE</option>
+                        <option>SP</option>
+                        <option>TO</option>
+                    </datalist>
+                    <span id="textEditProfile">Bairro <span className="redEmphasis">*</span></span>
+                    <input className="inputProfile" name="districtInput" ref={register({ required: true })} placeholder="Insira o nome de seu bairro"></input>
+                    {errors.districtInput && <span className="redEmphasis">Você precisa inserir o nome de seu bairro</span>}
+                    <span id="textEditProfile">Endereço <span className="redEmphasis">*</span></span>
+                    <input className="inputProfile" name="addressInput" ref={register({ required: true })} placeholder="Insira seu endereço"></input>
+                    {errors.addressInput && <span className="redEmphasis">Você precisa inserir seu endereço</span>}
+                    <span id="textEditProfile" >Número <span className="redEmphasis">*</span></span>
+                    <input className="inputProfile" name="addresNumberInput" ref={register({ required: true })} type="number" placeholder="Insira o número de sua residência"></input>
+                    {errors.addresNumberInput && <span className="redEmphasis">Você precisa inserir o número de sua residência</span>}
+                    <span id="textEditProfile">Complemento</span>
+                    <input className="inputProfile"  placeholder="Insira um complemento se achar necessário"></input>
+                </div>
+            </div>
+            <input name="formSubmitInput" type="submit" id="submitFormButton" label="Enviar"/>
+            {errors.formSubmitInput && <span className="redEmphasis">Verifique se preencheu corretamente todos os campos</span>}
+        </form>
+    );
 }
